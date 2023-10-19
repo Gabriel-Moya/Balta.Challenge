2cbd0def7d91be81b753308858d9a847cc90765a
thali <thali@DESKTOP-B5VRK6P>
2023-10-19 19:15:08 -0300

    feat: :sparkles: implementação do swagger

--- src/Balta.Challenge.Web/Program.cs
+++ src/Balta.Challenge.Web/Program.cs
@@ -1,7 +1,17 @@
 using Balta.Challenge.Web.Startup;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerConfiguration();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseSwaggerConfiguration();
 
 app.AddControllers();
 
---
+++ src/Balta.Challenge.Web/Startup/ConfigureSwagger.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Balta.Challenge.Web.Startup;
+
+public static class ConfigureSwagger
+{
+    public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Desafio Balta", Description = "Api criada para desafio Balta.", Version = "v1" });
+        });
+
+        return services;
+    }
+
+    public static WebApplication UseSwaggerConfiguration(this WebApplication app)
+    {
+        app.UseSwagger();
+
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiDesafioBalta v1");
+        });
+
+        app.MapGet("/", (HttpContext context) =>
+        {
+            context.Response.Redirect("/swagger");
+            return Task.CompletedTask;
+        });
+
+        return app;
+    }
+}
