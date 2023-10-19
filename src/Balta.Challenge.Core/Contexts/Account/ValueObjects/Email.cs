37785c5f6f6e05827cb2cd3e7eb593af13257d11
Gabriel Moya <gabrielmoya123@gmail.com>
2023-10-18 20:52:07 -0400

    chore: create solution

---
+++ src/Balta.Challenge.Core/Contexts/Account/ValueObjects/Email.cs
@@ -0,0 +1,30 @@
+﻿using Balta.Challenge.Core.Contexts.Account.ValueObjects.Exceptions;
+
+namespace Balta.Challenge.Core.Contexts.Account.ValueObjects;
+
+public readonly struct Email
+{
+    // Constructors
+    private Email(string address)
+    {
+        InvalidEmailException.ThrowIfIsInvalid(address);
+        Address = address;
+    }
+
+    // Public Properties
+    public string Address { get; } = null!;
+
+    // Public Methods
+    public static Email Create(string address)
+        => new (address);
+
+    // Overloads
+    public static implicit operator string(Email email)
+        => email.ToString();
+
+    public static implicit operator Email(string address)
+        => new (address);
+
+    public override string ToString()
+        => Address;
+}
---
+++ src/Balta.Challenge.Core/Contexts/Account/ValueObjects/Exceptions/InvalidEmailException.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Balta.Challenge.Core.Contexts.Account.ValueObjects.Exceptions;
+
+public partial class InvalidEmailException : Exception
+{
+    // Constants
+    private const string DefaultErrorMessage = "Email is not valid";
+
+    [GeneratedRegex("^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$")]
+    private static partial Regex Pattern();
+
+    // Constructors
+    public InvalidEmailException() : base(DefaultErrorMessage) { }
+    public InvalidEmailException(string? message) : base(message) { }
+    public InvalidEmailException(string? message, Exception? innerException) : base(message, innerException) { }
+
+    // Public Methods
+    public static void ThrowIfIsInvalid(string? address)
+    {
+        if (string.IsNullOrEmpty(address))
+            throw new InvalidEmailException();
+
+        address = address.ToLower().Trim();
+
+        if (address.Length < 5)
+            throw new InvalidEmailException();
+
+        if (!Pattern().IsMatch(address))
+            throw new InvalidEmailException();
+    }
+}
