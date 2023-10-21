114e87fb5d2ed20f9eb88713e01c37d93cd6be1b
Gabriel Moya <gabrielmoya123@gmail.com>
2023-10-21 19:34:08 -0400

    feature: implementing account controller and token jwt

--- src/Balta.Challenge.Core/Configuration.cs
+++ src/Balta.Challenge.Core/Configuration.cs
@@ -3,10 +3,16 @@
 public static class Configuration
 {
     public static PasswordConfiguration Password { get; set; } = new();
+    public static JwtConfiguration Jwt { get; set; } = new();
 
     public class PasswordConfiguration
     {
         public int Length { get; } = 8;
         public string Salt { get; } = string.Empty;
+    }
+
+    public class JwtConfiguration
+    {
+        public string JwtPrivateKey { get; } = string.Empty;
     }
 }
---
+++ src/Balta.Challenge.Web/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using Balta.Challenge.Web.Extensions;
+using MediatR;
+
+namespace Balta.Challenge.Web.Controllers;
+
+public static class AccountController
+{
+    public static void MapAccountEndpoints(this WebApplication app)
+    {
+        // Create
+        app.MapPost("/api/v1/accounts", async (
+            Core.Contexts.Account.UseCases.Create.Request request,
+            IRequestHandler<
+                Core.Contexts.Account.UseCases.Create.Request,
+                Core.Contexts.Account.UseCases.Create.Response> handler) =>
+        {
+            var result = await handler.Handle(request, new CancellationToken());
+            return result.IsSuccess
+                ? Results.Created($"/api/v1/account/{result.Data?.Id}", result.Data)
+                : Results.Json(result, statusCode: result.StatusCode);
+        });
+
+        // Authenticate
+        app.MapPost("/api/v1/authenticate", async (
+            Core.Contexts.Account.UseCases.Authenticate.Request request,
+            IRequestHandler<
+                Core.Contexts.Account.UseCases.Authenticate.Request,
+                Core.Contexts.Account.UseCases.Authenticate.Response> handler) =>
+        {
+            var result = await handler.Handle(request, new CancellationToken());
+
+            if (!result.IsSuccess)
+                return Results.Json(result, statusCode: result.StatusCode);
+
+            if (result.Data is null)
+                return Results.Json(result, statusCode: 500);
+
+            result.Data.Token = JwtExtensions.Generate(result.Data);
+            return Results.Ok(result);
+        });
+    }
+}
