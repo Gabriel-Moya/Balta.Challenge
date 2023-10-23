1e40eda2428a7bc6e90d13d62dba21203eb81226
Gabriel Moya <gabrielmoya123@gmail.com>
2023-10-22 21:32:21 -0400

    chore: remove hello world controller

--- src/Balta.Challenge.Web/Startup/ConfigureControllers.cs
+++ src/Balta.Challenge.Web/Startup/ConfigureControllers.cs
@@ -6,7 +6,6 @@ public static class ConfigureControllers
 {
     public static void AddControllers(this WebApplication app)
     {
-        HelloWorldController.HelloWorldDemo(app);
         AccountController.MapAccountEndpoints(app);
         LocaleController.MapLocaleEndpoints(app);
         // Add news controllers here
